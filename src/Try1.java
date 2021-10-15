7d89717936f7c167162574988624261a59938323
noridayumanshor <noridayumanshor@noridayus-mbp.lan>
2021-10-15 08:38:50 +0800

    Commit Kedua

--- src/Try1.java
+++ src/Try1.java
@@ -4,7 +4,7 @@ public class Try1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Try");
-		System.out.println("Test");
+		System.out.println("Test    ....");
 		
 	}
 
